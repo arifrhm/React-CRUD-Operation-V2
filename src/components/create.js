26b5d4af457d97d4f03f910051f797422a8945e5
arifrhm <arif.rahman.rhm@gmail.com>
2022-06-28 23:37:30 +0700

    Update validasi create data form kosong

--- src/components/create.js
+++ src/components/create.js
@@ -8,31 +8,58 @@ export default function Create() {
     const [firstName, setFirstName] = useState('');
     const [lastName, setLastName] = useState('');
     const [checkbox, setCheckbox] = useState(false);
+
+    // const [name, setMessage] = useState('');
+
+    const handleChangeFirstName = event => {
+        setFirstName(event.target.value);
+    };
+
+    const handleChangeLastName = event => {
+        setLastName(event.target.value);
+    };
+
+    const handleClick = event => {
+        event.preventDefault();
+
+        if ((firstName.trim().length !== 0 && lastName.trim().length !== 0)) {
+            axios.post(`https://60fbca4591156a0017b4c8a7.mockapi.io/fakeData`, {
+                firstName,
+                lastName,
+                checkbox
+            }).then(() => {
+                history.push('/read')
+            });
+        } else {
+        console.log('input value is empty');
+        }
+    };
+
     console.log(checkbox)
-    const postData = () => {
-        axios.post(`https://60fbca4591156a0017b4c8a7.mockapi.io/fakeData`, {
-            firstName,
-            lastName,
-            checkbox
-        }).then(() => {
-            history.push('/read')
-        })
-    }
+    // const postData = () => {
+    //     axios.post(`https://60fbca4591156a0017b4c8a7.mockapi.io/fakeData`, {
+    //         firstName,
+    //         lastName,
+    //         checkbox
+    //     }).then(() => {
+    //         history.push('/read')
+    //     })
+    // }
     return (
         <div>
             <Form className="create-form">
                 <Form.Field>
                     <label>First Name</label>
-                    <input placeholder='First Name' onChange={(e) => setFirstName(e.target.value)}/>
+                    <input placeholder='First Name' onChange={handleChangeFirstName}/>
                 </Form.Field>
                 <Form.Field>
                     <label>Last Name</label>
-                    <input placeholder='Last Name' onChange={(e) => setLastName(e.target.value)}/>
+                    <input placeholder='Last Name' onChange={handleChangeLastName}/>
                 </Form.Field>
                 <Form.Field>
                     <Checkbox label='I agree to the Terms and Conditions' onChange={(e) => setCheckbox(!checkbox)}/>
                 </Form.Field>
-                <Button onClick={postData} type='submit'>Submit</Button>
+                <Button onClick={handleClick} type='submit'>Submit</Button>
             </Form>
         </div>
     )
